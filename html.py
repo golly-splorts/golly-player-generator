b93746eec4a3535eb36f8957b6c1d95397d163bf
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2020-11-16 23:13:45 -0800

    add fixups for html output

--- html.py
+++ html.py
@@ -10,25 +10,30 @@ if not os.path.exists('pages'):
 
 for teamname, teamroster in roster.items():
 
+    tableclasses = "table table-bordered table-striped table-hover table-dark table-sm"
+
     html = []
     html.append('{% extends \'base.html\' %}')
     html.append('{% block content %}')
     html.append('<div class="container">')
-    html.append('<h1>%s Roster</h1>'%teamname)
-    html.append('<table class="table">')
+    html.append('<h1>%s <small>Team Roster</small></h1>'%teamname)
+    html.append('<table class="%s">'%tableclasses)
+    html.append('  <caption>%s Team Roster</caption>'%teamname)
     html.append('  <thead>')
     html.append('  <tr>')
+    html.append('    <th class="text-center">Row</th>')
+    html.append('    <th class="text-center">Col</th>')
     html.append('    <th>Player Name</th>')
-    html.append('    <th>Player Row</th>')
-    html.append('    <th>Player Col</th>')
     html.append('  </tr>')
     html.append('  </thead>')
     html.append('  <tbody>')
 
     sorted_team = sorted(teamroster,  key=(lambda x: (x[1], x[2])))
+    #sorted_team = sorted(teamroster,  key=(lambda x: x[0]))
     for player, row, col in sorted_team:
+        link = "https://example.com/example.html?p=%s&q=%s"%(row+1, col+1)
         html.append('    <tr>')
-        html.append('    <td>%s</td><td>%s</td><td>%s</td>'%(player, row, col))
+        html.append('    <td class="text-center">%s</td><td class="text-center">%s</td><td><a href="%s">%s</a></td>'%(row+1, col+1, link, player))
         html.append('    </tr>')
 
     html.append('  </tbody>')
